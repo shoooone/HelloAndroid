31d68fe8af8f650378552711863f4514f38cb700
s_watanabe <s_watanabe@dialonet.co.jp>
2019-02-21 16:08:30 +0900

    button click event & ImageView

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.helloandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tapHere = findViewById<Button>(R.id.tapHere)
+        tapHere.setOnClickListener {
+            val textView = findViewById<TextView>(R.id.textView)
+            textView.text = "ボタンがタップされました"
+        }
     }
 }
